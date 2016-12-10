49095c70c4036a51d8c4d359ffdc0aca7b526c4f
kcrong <kcrong@kim82536.pe.kr>
2016-12-10 20:24:46 +0900

    Remove example plugin

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,6 +5,6 @@ import importlib
 plugin_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, plugin_path)
 
-plugin_directory_list = [_ for _ in os.listdir(plugin_path) if _.startswith("__") is False]
+plugin_directory_list = [_ for _ in os.listdir(plugin_path) if _.startswith("__") is False and _ != 'example']
 
 plugin_list = [importlib.import_module(plugin) for plugin in plugin_directory_list]
