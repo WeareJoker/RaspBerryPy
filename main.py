3784bdf80a51e96aa861f40b9a3858cb6a18fa10
kcrong <kcrong@kim82536.pe.kr>
2016-12-10 20:48:29 +0900

    Now use scapy instead of ImpacketDecoder

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 import dpkt
-from impacket import ImpactDecoder
+from scapy.layers.l2 import Ether
 
 from plugins import plugin_list
 
@@ -10,8 +10,8 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 filename = raw_input("Input PCAP filename: ")
 
-decoder = ImpactDecoder.EthDecoder()
 
 for ts, pkt in dpkt.pcap.Reader(open(filename, 'r')):
+    packet = Ether(pkt)
     for plugin in plugin_list:
-        plugin.handler(decoder.decode(pkt))
+        plugin.handler(packet)
