6696061d242ed44aa713d4b6f104a71128b28325
kcrong <kcrong@kim82536.pe.kr>
2016-12-10 21:05:47 +0900

    ReChange to ImpactDecoder

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import os
 import sys
 
 import dpkt
-from scapy.layers.l2 import Ether
+from impacket import ImpactDecoder
 
 from plugins import plugin_list
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+decoder = ImpactDecoder.EthDecoder()
+
 filename = raw_input("Input PCAP filename: ")
 
-
 for ts, pkt in dpkt.pcap.Reader(open(filename, 'r')):
-    packet = Ether(pkt)
+    packet = decoder.decode(pkt)
     for plugin in plugin_list:
         plugin.handler(packet)
