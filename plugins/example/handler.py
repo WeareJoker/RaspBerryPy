53ab255dd8a3780bd9f50c70fc41d2875d89a8b7
kcrong <kcrong@kim82536.pe.kr>
2016-12-10 20:20:07 +0900

    Add exception with get_th_dport

--- plugins/example/handler.py
+++ plugins/example/handler.py
@@ -6,7 +6,12 @@ session = Session()
 def handler(ether):
     ip = ether.child()
     proto = ip.child()
-    if proto.get_th_dport() == 80:  # if http packet
-        data = proto.child()
-        print data.get_buffer_as_string()
+    try:
+        port = proto.get_th_dport()
+    except AttributeError:
+        pass
+    else:
+        if port == 80:  # if http packet
+            data = proto.child()
+            print data.get_buffer_as_string()
 
