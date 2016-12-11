86d4da67a55c3589ab9893d57d142a76e54dc82e
kcrong <kcrong@kim82536.pe.kr>
2016-12-12 03:40:48 +0900

    Change design main.py

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from plugins import plugin_list
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-filename = input("Input PCAP filename: ")
+
+def analysis(pcap_filename):
+    for pkt in rdpcap(pcap_filename):
+        for plugin in plugin_list:
+            plugin.handler(pkt)
 
 
-for pkt in rdpcap(filename):
-    for plugin in plugin_list:
-        plugin.handler(pkt)
+if __name__ == '__main__':
+    analysis(input("PCAP: "))
