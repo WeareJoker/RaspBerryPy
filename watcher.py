61b958ae4ebfd03f679af083dda2e7a26e42b01e
kcrong <kcrong@kim82536.pe.kr>
2016-12-12 19:38:02 +0900

    Remove filename argv Add Exception at parse filetype

--- watcher.py
+++ watcher.py
@@ -10,16 +10,22 @@ from main import analysis
 class PcapHandler(FileSystemEventHandler):
     analysis_process_list = list()
 
-    def __init__(self, observer, filename):
+    def __init__(self, observer):
         self.observer = observer
-        self.filename = filename
 
     def on_created(self, event):
         if not event.is_directory:  # and event.src_path.endswith(self.filename):
             filename = os.path.basename(event.src_path)
             print("%s created" % filename)
-            if filename.split('.')[-1] != 'pcap':
+
+            try:
+                filetype = filename.split('.')[-1]
+            except IndexError:
                 return
+            else:
+                if 'pcap' not in filetype:
+                    return
+
             print("Start Analysis %s" % filename)
             # analysis(os.path.basename(event.src_path))
             p = Process(target=analysis, args=(filename,))
@@ -27,13 +33,12 @@ class PcapHandler(FileSystemEventHandler):
             PcapHandler.analysis_process_list.append(p)
 
 
-def main(argv):
+def main():
     from config import project_path
     path = project_path
-    filename = argv[1]
 
     observer = Observer()
-    event_handler = PcapHandler(observer, filename)
+    event_handler = PcapHandler(observer)
 
     observer.schedule(event_handler, path, recursive=False)
     observer.start()
@@ -41,12 +46,9 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("%s [pcap name]" % sys.argv[0])
-        exit(1)
 
     try:
-        main(sys.argv)
+        main()
     except KeyboardInterrupt:
         for analysis_process in PcapHandler.analysis_process_list:
             analysis_process.join()
