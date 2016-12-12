02d10fc7a937f7a72a7e7f9957d5bed8a48f7b4f
kcrong <kcrong@kim82536.pe.kr>
2016-12-12 21:57:59 +0900

    Add user-input command type, parser

--- main.py
+++ main.py
@@ -3,18 +3,44 @@ from config import analysis_path, project_path, plugin_path
 
 sys.path.extend([plugin_path, project_path, analysis_path])
 
-from analysis.analyser import migrate, analysis
+from analysis.analyser import migrate, analysis_pcap, analysis_sniff
+
+
+def from_interface():
+    try:
+        interface_name = sys.argv[2]
+    except IndexError:
+        show_help()
+        return
+    else:
+        analysis_sniff(interface_name)
+
+
+def from_pcap():
+    try:
+        pcap_name = sys.argv[2]
+    except IndexError:
+        show_help()
+    else:
+        analysis_pcap(pcap_name)
+
+
+def show_help():
+    self_name = sys.argv[0]
+    print("Available Command: ")
+    print("%s migrate" % self_name)
+    print("%s interface [interface to sniff & analysis]" % self_name)
+    print("%s pcap [pcap filename to analysis" % self_name)
+
 
 if __name__ == '__main__':
     try:
-        argv = sys.argv[1]
-
-    except IndexError:
-        print("%s [ migrate || filename ]" % sys.argv[0])
-        print("Requires more than one argv.")
-
+        func = {
+            'migrate': migrate,
+            'interface': from_interface,
+            'pcap': from_pcap
+        }[sys.argv[1]]
+    except (IndexError, KeyError):
+        show_help()
     else:
-        if argv == "migrate":
-            migrate()
-        else:
-            analysis(argv)
+        func()
