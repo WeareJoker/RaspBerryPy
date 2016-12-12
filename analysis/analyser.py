2cede306be3e57c4a20682917380ad5acd00f55f
kcrong <kcrong@kim82536.pe.kr>
2016-12-12 21:57:40 +0900

    Add analysis type (pcap, sniff)

--- analysis/analyser.py
+++ analysis/analyser.py
@@ -1,11 +1,21 @@
 from scapy.all import *
 from analysis.plugins import plugin_list
+from multiprocessing import Process
 
 
-def analysis(pcap_filename):
+def packet_handler(pkt):
+    for plugin in plugin_list:
+        p = Process(target=plugin.handler, args=(pkt,))
+        p.start()
+
+
+def analysis_pcap(pcap_filename):
     for pkt in rdpcap(pcap_filename):
-        for plugin in plugin_list:
-            plugin.handler(pkt)
+        packet_handler(pkt)
+
+
+def analysis_sniff(interface):
+    sniff(iface=interface, prn=packet_handler)
 
 
 def migrate():
