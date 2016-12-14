7fbf2e6d25f1de63a6dee97fdc95970191dd46c8
kcrong <kcrong@kim82536.pe.kr>
2016-12-14 21:00:38 +0900

    Make db path to absolute

--- analysis/database.py
+++ analysis/database.py
@@ -3,8 +3,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import project_path
+from os.path import join
 
 # engine = create_engine('sqlite:///result.db', echo=True)
-engine = create_engine('sqlite:///result.db')
+engine = create_engine('sqlite:///' + join(project_path, 'result.db'))
 Base = declarative_base(engine)
 Session = sessionmaker(bind=engine)
