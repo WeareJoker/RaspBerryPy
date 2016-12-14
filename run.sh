2fab25bf92fbfba6abe29403207355d08ab912a7
asdf <asdf@asdf.com>
2016-12-14 15:54:40 +0000

    Add tcpdump install

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install -y build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libxml2 libxml2-dev libxslt1-dev python-dev libxml2-dev libxslt1-dev zlib1g-dev
+sudo apt-get install -y tcpdump build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libxml2 libxml2-dev libxslt1-dev python-dev libxml2-dev libxslt1-dev zlib1g-dev
 
 /bin/bash ./shellscript/dummy.sh add tap0
 
